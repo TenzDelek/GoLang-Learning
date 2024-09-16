0dd0989052e405e7b38ef05b5a12ac6b658fe0ee
TenzDelek <tibetdelek@gmail.com>
2024-09-16 21:43:46 +0530

    ifelse

--- 10struct/main.go
+++ 10struct/main.go
@@ -4,10 +4,10 @@ import "fmt"
 
 func main() {
 	fmt.Println("Hello World")
-	tenzin := User{"tenzin", "tenzindelek@gmail.com", true, 22}
-	fmt.Println(tenzin)
-	fmt.Printf("the detailed are %+v\n", tenzin) //gives the detailed like name:tenzin
-	fmt.Printf("name is %v and email is %v", tenzin.Name, tenzin.Email)
+	tenz := User{"dummy", "dummy@gmail.com", true, 22}
+	fmt.Println(tenz)
+	fmt.Printf("the detailed are %+v\n", tenz) //gives the detailed like name:tenz
+	fmt.Printf("name is %v and email is %v", tenz.Name, tenz.Email)
 }
 
 type User struct {
---
+++ 11ifelse/main.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("if else")
+	var res string
+	logincount := 24
+	if logincount < 10 {
+		res = "Regular User"
+	} else if logincount > 10 {
+		res = "top G"
+	} else { //remember that else if and else should be after the { if declare below { not consider by lexar
+		res = "admin"
+	}
+
+	fmt.Println(res)
+
+	if 9%2 == 0 {
+		fmt.Println("number is even")
+	} else {
+		fmt.Println("number is odd")
+	}
+
+	//assign on the go and check
+	if num := 3; num < 5 {
+		fmt.Println("less than 5")
+	} else {
+		fmt.Println("greater than 5")
+	}
+
+	//nil like python we have None
+}
